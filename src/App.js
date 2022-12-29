aa1a1e10f0f758112b9a93ce69348829212d6653
azboss2021 <alexanderzyn1@gmail.com>
2022-12-28 21:36:09 -0800

    Extremely buggy handle clicks. Not at all working version of app. Still requires styling. Implemeneted fisher yates shuffle for card randomizing.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { useState } from 'react';
 import './App.css';
 import Cards from './components/Cards';
+import Score from './components/Score';
 
 function App() {
-  const [numberOfCards, setNumberOfCards] = useState(12);
+  const [score, setScore] = useState(0);
+  const [numberOfCards, setNumberOfCards] = useState(14);
+  const [cards, setCards] = useState([]);
 
   return (
     <>
-      <Cards numberOfCards={numberOfCards} />
+      <Score score={score} />
+      <Cards
+        numberOfCards={numberOfCards}
+        cards={cards}
+        setCards={setCards}
+        score={score}
+        setScore={setScore}
+      />
     </>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,42 +2,96 @@ import React, { useEffect, useState } from 'react';
 import uniqid from 'uniqid';
 import CharacterJSON from '../Characters.json';
 
-const Cards = ({ numberOfCards }) => {
-  const [cards, setCards] = useState([]);
+const Cards = ({ numberOfCards, cards, setCards, score, setScore }) => {
+  const [seenCards, setSeenCards] = useState([]);
+  const [currentCard, setCurrentCard] = useState('');
 
   useEffect(() => {
+    setCards([]);
     const chars = CharacterJSON;
-    let alreadySeen = [];
+    let newSet = [];
+    let seen = [];
 
     for (let i = 0; i < numberOfCards; i++) {
       const rand = Math.floor(Math.random() * chars.length);
-      if (alreadySeen.includes(rand)) {
+      if (seen.includes(rand)) {
         i--;
         continue;
       } else {
         const newItem = chars[rand];
-        setCards((current) => [...current, newItem]);
+        newItem.id = uniqid();
+        newSet.push(newItem);
       }
-      alreadySeen.push(rand);
+      seen.push(rand);
     }
+
+    setCards([...newSet]);
   }, []);
 
+  useEffect(() => {
+    document.querySelector('#score').textContent = score;
+    console.log(score);
+  }, [score]);
+
+  useEffect(() => {
+    console.log(seenCards);
+  }, [seenCards]);
+
+  const FisherYatesShuffle = (arr) => {
+    let m = arr.length,
+      t,
+      i;
+
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+
+      t = arr[m];
+      arr[m] = arr[i];
+      arr[i] = t;
+    }
+
+    return arr;
+  };
+
+  const HandleClick = (id) => {
+    setSeenCards((current) => [...current, id]);
+    setCards([...FisherYatesShuffle(cards)]);
+    if (seenCards.includes(id)) {
+      setScore(0);
+      setSeenCards([]);
+      setCards([...FisherYatesShuffle(cards)]);
+      return;
+    } else {
+      setScore(score + 1);
+    }
+  };
+
   return (
-    <div>
-      {cards.map((card) => {
-        return (
-          <div key={uniqid()}>
-            <img
-              src={require('../images/' + card.img)}
-              alt={card.name}
-              width="64px"
-              height="64px"
-            ></img>
-            {card.name}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div id="cards">
+        {cards.map((card) => {
+          return (
+            <div
+              id={card.id}
+              key={card.id}
+              className="card"
+              onClick={(e) => HandleClick(e.target.id)}
+            >
+              <img
+                src={require('../images/' + card.img)}
+                alt={card.name}
+                width="64px"
+                height="64px"
+              ></img>
+              <div className="card_name">
+                {card.name}, {card.id}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div>{seenCards}</div>
+    </>
   );
 };
 
